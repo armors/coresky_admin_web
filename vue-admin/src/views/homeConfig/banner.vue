3080146de7f6d16ebebfc8b6afa7cb864b162c39
yanlei <464625933@qq.com>
2023-01-09 18:32:04 +0800

    首页配置

--- vue-admin/src/views/homeConfig/banner.vue
+++ vue-admin/src/views/homeConfig/banner.vue
@@ -1,46 +1,16 @@
 <template>
   <div>
-    <el-form
-      :model="queryParams"
-      ref="queryForm"
-      size="small"
-      :inline="true"
-      label-width="72px"
-    >
+    <el-form :model="queryParams" ref="queryForm" size="small" :inline="true" label-width="72px">
       <el-form-item label="">
-        <el-input
-          v-model="queryParams.areaNm"
-          placeholder="请输入标题名称"
-          clearable
-          @keyup.enter.native="handleQuery"
-        />
+        <el-input v-model="queryParams.areaNm" placeholder="请输入标题名称" clearable @keyup.enter.native="handleQuery" />
       </el-form-item>
       <el-form-item>
-        <el-button
-          type="primary"
-          icon="el-icon-search"
-          size="mini"
-          @click="handleQuery"
-          >搜索</el-button
-        >
-        <el-button
-          type="primary"
-          plain
-          icon="el-icon-plus"
-          size="mini"
-          @click="handleAdd"
-          >新增</el-button
-        >
+        <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
+        <el-button type="primary" plain icon="el-icon-plus" size="mini" @click="handleAdd">新增</el-button>
       </el-form-item>
     </el-form>
     <el-table v-loading="loading" :data="dataList" size="small">
-      <el-table-column
-        label="序号"
-        align="center"
-        type="index"
-        width="70"
-        fixed
-      />
+      <el-table-column label="序号" align="center" type="index" width="70" fixed />
       <el-table-column label="主标题" align="center" prop="title" />
       <el-table-column label="子标题" align="center" prop="subtitle" />
       <el-table-column label="跳转链接" align="center" prop="url" width="260" />
@@ -52,99 +22,47 @@
       <el-table-column label="权重" align="center" prop="weight" />
       <el-table-column label="启用状态" align="center" prop="enable">
         <template slot-scope="scope">
-          <el-switch
-            :active-value="1"
-            :disabled="true"
-            :inactive-value="0"
-            v-model="scope.row.enable"
-          >
+          <el-switch :active-value="1" :disabled="true" :inactive-value="0" v-model="scope.row.enable">
           </el-switch>
         </template>
       </el-table-column>
-      <el-table-column
-        label="操作"
-        align="center"
-        class-name="small-padding fixed-width"
-      >
+      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
         <template slot-scope="scope">
-          <el-button
-            size="mini"
-            type="text"
-            icon="el-icon-edit"
-            @click="handleUpdate(scope.row)"
-            >修改</el-button
-          >
-          <el-button
-            size="mini"
-            type="text"
-            icon="el-icon-edit"
-            @click="handleDelete(scope.row)"
-            >删除</el-button
-          >
+          <el-button size="mini" type="text" icon="el-icon-edit" @click="handleUpdate(scope.row)">修改</el-button>
+          <el-button size="mini" type="text" icon="el-icon-edit" @click="handleDelete(scope.row)">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
 
-    <el-dialog
-      title="首页开屏区配置"
-      :visible.sync="open"
-      width="600px"
-      append-to-body
-    >
-      <el-form
-        ref="form"
-        :model="form"
-        :rules="rules"
-        label-width="120px"
-        size="small"
-        style="margin-right: 45px"
-      >
+    <el-dialog title="首页开屏区配置" :visible.sync="open" width="600px" append-to-body>
+      <el-form ref="form" :model="form" :rules="rules" label-width="120px" size="small" style="margin-right: 45px">
         <el-form-item label="主标题" prop="title">
           <el-input v-model="form.title" placeholder="请输入主标题" />
         </el-form-item>
         <el-form-item label="子标题" prop="subtitle">
-          <el-input
-            v-model="form.subtitle"
-            type="textarea"
-            :rows="3"
-            placeholder="请输入子标题内容"
-          />
+          <el-input v-model="form.subtitle" type="textarea" :rows="3" placeholder="请输入子标题内容" />
         </el-form-item>
         <el-form-item label="链接地址" prop="url" :show-overflow-tooltip="true">
           <el-input v-model="form.url" placeholder="请输入链接地址" />
         </el-form-item>
         <el-form-item label="图片" prop="image">
-          <el-upload
-            class="avatar-uploader"
-            :action="uploadAction"
-            accept="image/jpg, image/jpeg, image/png, image/gif"
-            :show-file-list="false"
-            :on-success="uploadSuccess"
-            :before-upload="beforeUpload"
-          >
-            <img
-              v-if="form.image"
-              :src="STATIC_URL + form.image"
-              class="avatar"
-            />
+          <el-upload class="avatar-uploader" :action="uploadAction" accept="image/jpg, image/jpeg, image/png, image/gif"
+            :show-file-list="false" :on-success="uploadSuccess" :before-upload="beforeUpload">
+            <img v-if="form.image" :src="STATIC_URL + form.image" class="avatar" />
             <i v-else class="el-icon-plus avatar-uploader-icon"></i>
           </el-upload>
         </el-form-item>
         <el-form-item label="是否启用" prop="useMark">
-          <el-switch
-            :active-value="1"
-            :inactive-value="0"
-            v-model="form.enable"
-          >
+          <el-switch :active-value="1" :inactive-value="0" v-model="form.enable">
           </el-switch>
         </el-form-item>
+        <el-form-item label="兑换有效期">
+          <el-date-picker style="width: 100%;" @change="dateChange()" value-format="yyyy-MM-dd HH:mm:ss" v-model="myDatetime"
+            type="datetimerange" range-separator="至" start-placeholder="开始日期" end-placeholder="结束日期">
+          </el-date-picker>
+        </el-form-item>
         <el-form-item label="权重" prop="cOrderCd">
-          <el-input-number
-            style="width: 100%"
-            v-model="form.weight"
-            controls-position="right"
-            :min="0"
-          />
+          <el-input-number style="width: 100%" v-model="form.weight" controls-position="right" :min="0" />
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
@@ -158,11 +76,13 @@
 <script>
 import { advList, advCreate, advDelete, advUpdate } from "@/api/common";
 import { regPrice } from "@/utils/reg";
+import dayjs from 'dayjs'
+
 import base from "@/mixins/base";
 export default {
   name: "banner",
   mixins: [base],
-  data() {
+  data () {
     return {
       // 遮罩层
       loading: false,
@@ -191,9 +111,8 @@ export default {
         weight: "",
         url: "",
         enable: "",
-
-        // startTime: '',
-        // endTiem: '',
+        startTime: '',
+        endTime: '',
         // contract: '',
       },
       imageUrl: "",
@@ -208,13 +127,24 @@ export default {
       },
       selectDeptType: "search",
       dialogStatus: "",
+      myDatetime: null,
     };
   },
-  created() {
+  created () {
     this.getList();
   },
   methods: {
-    uploadSuccess(res) {
+    dateChange () {
+      if (this.myDatetime != null) {
+        this.form.startTime = dayjs(this.myDatetime[0]).unix()
+        this.form.endTime = dayjs(this.myDatetime[1]).unix()
+      }
+      else {
+        this.form.startTime = ''
+        this.form.endTime = ''
+      }
+    },
+    uploadSuccess (res) {
       console.log(this.IMG_URL);
       this.successUploadHandle();
       if (res.errno === 0) {
@@ -225,7 +155,7 @@ export default {
       }
     },
     /** 查询岗位列表 */
-    getList() {
+    getList () {
       this.loading = true;
       advList(this.queryParams).then((response) => {
         this.dataList = response.data;
@@ -233,12 +163,12 @@ export default {
       });
     },
     // 取消按钮
-    cancel() {
+    cancel () {
       this.open = false;
       this.reset();
     },
     // 表单重置
-    reset() {
+    reset () {
       this.form = {
         id: "",
         advId: "1",
@@ -248,22 +178,24 @@ export default {
         weight: "",
         url: "",
         enable: "",
+        startTime: '',
+        endTime: ''
       };
     },
     /** 搜索按钮操作 */
-    handleQuery() {
+    handleQuery () {
       this.queryParams.pageNum = 1;
       this.getList();
     },
     /** 新增按钮操作 */
-    handleAdd() {
+    handleAdd () {
       this.dialogStatus = "create";
       this.reset();
       this.open = true;
     },
 
     /** 提交按钮 */
-    submitForm() {
+    submitForm () {
       this.$refs["form"].validate((valid) => {
         if (!valid) return;
         if (this.form.id) {
@@ -281,8 +213,14 @@ export default {
         }
       });
     },
-    handleUpdate(row) {
+    handleUpdate (row) {
       this.form = Object.assign({}, row);
+      if (!!this.form.startTime && !!this.form.endTime) {
+        this.myDatetime = [
+          dayjs.unix(this.form.startTime).format('YYYY-MM-DD HH:mm:ss'),
+          dayjs.unix(this.form.endTime).format('YYYY-MM-DD HH:mm:ss')
+        ]
+      }
       this.dialogStatus = "update";
       this.open = true;
       this.$nextTick(() => {
@@ -290,7 +228,7 @@ export default {
       });
     },
     /** 删除按钮操作 */
-    handleDelete(row) {
+    handleDelete (row) {
       this.$modal
         .confirm("是否确认删除？")
         .then(function () {
@@ -300,7 +238,7 @@ export default {
           this.getList();
           this.$modal.msgSuccess("删除成功");
         })
-        .catch(() => {});
+        .catch(() => { });
     },
   },
 };
